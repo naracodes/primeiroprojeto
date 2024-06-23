22e33d2a16dff8178920c9dc3de3635ded93515b
Nara Alencar <naracodes@gmail.com>
2024-06-23 13:18:15 -0300

    Adicionando uma imagem e utilizando variaveis

--- App.js
+++ App.js
@@ -1,5 +1,5 @@
 import React, { Component } from "react";
-import { View, Text } from "react-native";
+import { View, Text, Image } from "react-native";
 
 function App(){
   return(
@@ -11,10 +11,27 @@ function App(){
 
 class App2 extends Component{
   render(){
+
+    let profissao = 'Desenvolvedora Full-stack';
+    let img = 'https://services.meteored.com/img/article/inteligencia-artificial-aprende-a-reconstruir-imagens-vistas-por-pessoas-ciencia-fotos-1679175318563_1280.jpg';
+
     return(
       <View>
         <Text>Olá, Mundo!</Text>
-        <Text>Meu primeiro app.</Text>
+        {/*Adicionando uma propriedade style no Text*/}
+        <Text style={{ color: 'red', fontSize: 25, margin: 15 }}>Meu primeiro app.</Text>
+        <Text style={{ fontSize: 18, color: 'blue' }}>
+          Edinara Alencar
+        </Text>
+
+        <Image
+         source={{ uri: img }}
+         style={{ width: 300, height: 300 }}
+        />
+
+        <Text style={{ fontSize: 18 }}>
+          {profissao}
+        </Text>
       </View>
       
     );
