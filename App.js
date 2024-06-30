d49b086b330bc0d5c21c8c078e7487086b1dad9f
Nara Alencar <naracodes@gmail.com>
2024-06-30 16:36:39 -0300

    Recebendo dados do usuario com TextInput

--- App.js
+++ App.js
@@ -1,15 +1,46 @@
-import React from "react";
-import { View, Text } from "react-native";
+import React, { useState } from "react";
+import { View, Text, TextInput, StyleSheet } from "react-native";
 
 function App(){
 
+  const [nome, setNome] = useState('');
+
+  function pegaNome(texto) {
+    if(texto.length > 0){
+      setNome('Bem vindo(a) ' + texto);
+    }else{
+      setNome('');
+    }
+  }
+
   return(
-    <View style={{ flex: 1, backgroundColor: '#121212' }}>
-      <View style={{ height: 60, backgroundColor: '#121212' }}></View>
-      <View style={{ flex: 1, backgroundColor: 'gray' }}></View>
-      <View style={{ height: 60, backgroundColor: '#121212' }}></View>
+    <View style={styles.container}>
+      <TextInput
+        style={styles.input}
+        placeholder="Digite seu nome"
+        onChangeText={ (text) => pegaNome(text) }
+      />
+
+      <Text style={styles.texto}>{nome}</Text>
+ 
     </View>
   );
 }
 
+const styles = StyleSheet.create({
+  container:{
+    flex:1
+  },
+  input:{
+    height: 45,
+    borderWidth: 1,
+    margin: 10,
+    padding: 10,
+    fontSize: 20
+  },
+  texto:{
+    textAlign: 'center',
+    fontSize: 25
+  }
+})
 export default App;
